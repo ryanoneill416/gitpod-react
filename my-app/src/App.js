cb189531fe53d24c0709d6d7c8e325ffbee6992a
Ryan O'Neill <ryanoneill416@gmail.com>
2022-12-13 20:00:58 +0000

    Initiate react app and produce first function using props

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="12"/>
+    </div>
+  );
+}
+
+export default App;
